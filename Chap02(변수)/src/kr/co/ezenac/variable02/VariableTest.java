c5b2ed0d1948405e8b5ef0251384e3985a557a80
bong2ee <qhduddlfkda@gmail.com>
2022-12-22 16:45:59 +0900

    Chap02

---
+++ Chap02(변수)/src/kr/co/ezenac/variable02/VariableTest.java
@@ -0,0 +1,17 @@
+package kr.co.ezenac.variable02;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		byte b1 = 127;
+		System.out.println(b1);
+		
+		//int num = 12345678900;
+		//long num = 12345678900;
+		long num = 12345678900L;
+		System.out.println(num);
+		
+	
+	}
+
+}
---
+++ Chap02(변수)/src/kr/co/ezenac/variable03/BooleanTest.java
@@ -0,0 +1,13 @@
+package kr.co.ezenac.variable03;
+
+public class BooleanTest {
+
+	public static void main(String[] args) {
+	
+		boolean isMarried = true;
+		System.out.println(isMarried);
+		
+
+	}
+
+}
