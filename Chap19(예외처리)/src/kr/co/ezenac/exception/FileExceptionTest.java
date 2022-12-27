05d4eb58c3622458d9c319d04db2e3c296d0fbe8
bong2ee <qhduddlfkda@gmail.com>
2022-12-27 13:05:28 +0900

    Chap19

---
+++ Chap19(예외처리)/src/kr/co/ezenac/exception/FileExceptionTest.java
@@ -0,0 +1,31 @@
+package kr.co.ezenac.exception;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileExceptionTest {
+	
+	public static void main(String[] args) {
+		
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream("ezen.txt");	
+		} catch (FileNotFoundException e) {	
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			System.out.println(e.getMessage());
+		} finally {
+			if(fis != null) {
+			try {
+				fis.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				}
+			}
+			System.out.println("항상 수행합니다.");
+		}
+		System.out.println("여기도 정상 수행합니다.");
+	}
+}
